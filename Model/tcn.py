5e552f9de11813bc6eabb301265e757813b38c90
tisharSinha <tisharsinha3@gmail.com>
2022-03-24 10:50:47 +0100

    TCN

--- Model/tcn.py
+++ Model/tcn.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.utils import weight_norm
 from torchsummary import summary
+import torch.optim as optim
 
 
 class Chomp1d(nn.Module):
@@ -62,16 +63,72 @@ class TCN_net(nn.Module):
         self.network = nn.Sequential(*layers)
 
 
-n_inputs = 5
+n_inputs = 32
 n_channels = [32, 32, 32, 32, 32]
-n_outputs = 5
-kernel_size = 7
+n_outputs = 32
+kernel_size = 2
 stride = 1
 dropout = 0.1
 
 net = TCN_net(n_inputs, n_channels, kernel_size, stride, dropout)
 
-summary(net, (5, 32, 1000), device='cpu')  # (in_channels, height, width)
+# summary(net, torch.zeros((32,1000))) # (in_channels, height, width)
 
-# stride, padding, dilation????
-# dropout???
+'''
+# Training parameters
+learning_rate = 5e-1  # step size for gradient descent
+epochs = 10  # how many times to iterate through the intire training set
+
+# Initialize network
+net = Net(layers)
+
+# Define list to store loss of each iteration
+train_losses = []
+train_accs = []
+
+for epoch in range(epochs):
+    # Training loop
+    for i, (x_batch, y_batch) in enumerate(train_loader):
+        # Flatten input to 1D tensor
+        x_batch = x_batch.flatten(start_dim=1)
+        # Convert labels to one-hot encoding
+        y_batch = nn.functional.one_hot(y_batch, num_classes=10)
+
+        # Perform forward pass
+        y_pred = net.forward(x_batch)
+
+        ########################################################################
+        #      TODO: Calculate Cross Entropy loss between prediction and       #
+        #                     labels and append to list.                       #
+        ########################################################################
+
+        pass
+
+        ########################################################################
+        #                          END OF YOUR CODE                            #
+        ########################################################################
+
+
+        ########################################################################
+        #            TODO: Perform backward pass and optimizer step.           #
+        ########################################################################
+
+        pass
+
+        ########################################################################
+        #                          END OF YOUR CODE                            #
+        ########################################################################
+
+        # Calculate accuracy of prediction
+        correct = torch.argmax(y_pred, axis=1) == torch.argmax(y_batch, axis=1)
+        train_accs.append(torch.sum(correct)/len(y_pred))
+
+        # Print progress
+        if i % 200 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch + 1, i * len(x_batch), len(train_loader.dataset),
+                100. * i / len(train_loader), loss))
+
+
+
+'''
