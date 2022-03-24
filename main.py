00e96d4d9a0f11c09e4e9008b5e86665d0978b9a
MarnixE <61150122+MarnixE@users.noreply.github.com>
2022-03-24 15:25:38 +0100

    Working on TripletDataset

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from data import data
-from model import *
+from model import tcn
 from utils import *
 
 DE = data.DataExtraction()
 
 X,Y,Z = DE.dataset_split()
+
+
+model = tcn.TCN_net()
+
+n_inputs = 32
+n_channels = [32, 32, 32, 32, 32]
+n_outputs = 32
+kernel_size = 2
+stride = 1
+dropout = 0.1
+
+net = tcn.TCN_net(n_inputs, n_channels, kernel_size, stride, dropout)
+
+
 
