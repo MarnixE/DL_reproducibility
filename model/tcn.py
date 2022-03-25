f8a89d3d11b430eb85825ee59e38a6a254217d43
Marcus_Malak <m.m.h.g.malak@student.tudelft.nl>
2022-03-25 15:42:28 +0100

    Moved training loop to main

--- model/tcn.py
+++ model/tcn.py
@@ -62,14 +62,6 @@ class TCN_net(nn.Module):
         self.network = nn.Sequential(*layers)
 
 
-n_inputs = 32
-n_channels = [32, 32, 32, 32, 32]
-n_outputs = 32
-kernel_size = 2
-stride = 1
-dropout = 0.1
-
-net = TCN_net(n_inputs, n_channels, kernel_size, stride, dropout)
 
 def CrossEntropyLoss(y_true, y_pred):
     
@@ -99,41 +91,8 @@ def Softmax(z):
     return p
 
 
-# Training parameters
-learning_rate = 5e-1  # step size for gradient descent
-epochs = 10  # how many times to iterate through the intire training set
 
 
-# Define list to store loss of each iteration
-train_losses = []
-train_accs = []
-
-for epoch in range(epochs):
-    # Training loop
-    for i, (x_batch, y_batch) in enumerate(train_loader):
-        # Flatten input to 1D tensor
-        x_batch = x_batch.flatten(start_dim=1)
-        # Convert labels to one-hot encoding
-        y_batch = nn.functional.one_hot(y_batch, num_classes=10)
-
-        # Perform forward pass
-        y_pred = net.forward(x_batch)
-
-        loss, grad = CrossEntropyLoss(y_batch, y_pred)
-        train_losses.append(loss)
-        
-        net.backward(grad)
-        net.optimizer_step(learning_rate)
-
-        # Calculate accuracy of prediction
-        correct = torch.argmax(y_pred, axis=1) == torch.argmax(y_batch, axis=1)
-        train_accs.append(torch.sum(correct)/len(y_pred))
-
-        # Print progress
-        if i % 200 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch + 1, i * len(x_batch), len(train_loader.dataset),
-                100. * i / len(train_loader), loss))
 
 
 
