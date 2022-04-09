3223e438b007463f91ec1fe4d2850c00fca20075
Marcus_Malak <m.m.h.g.malak@student.tudelft.nl>
2022-04-09 12:10:45 +0200

    Trained model

--- drive2vec/main.py
+++ drive2vec/main.py
@@ -10,6 +10,7 @@ import utils
 import numpy as np
 from IPython.display import display
 from torchsummary import summary
+import matplotlib.pyplot as plt
 
 
 torch.manual_seed(2020)
@@ -36,11 +37,11 @@ model.cuda()
 
 # Load data
 dp = data_process.DataProcess()
-train_loader = DataLoader(dp, batch_size=18, num_workers=5)
+train_loader = DataLoader(dp, batch_size=18, shuffle=True)
 
-# train_features, train_labels = next(iter(train_loader))
-# print(f"Feature batch shape: {train_features.size()}")
-# print(f"Labels batch shape: {train_labels.size()}")
+train_features, train_pos, train_neg,  train_labels = next(iter(train_loader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
 
 
 
@@ -71,90 +72,75 @@ def evaluate_accuracy(data_loader, net, device=device):
 
 
 # Loss function and optimizer
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+optimizer = optim.Adam(model.parameters(), lr=0.05)
 criterion = torch.jit.script(triplet_loss.TripletLoss())
 
 # Training parameters
-epochs = 100 
+epochs = 2000
 
 train_accs = []
 running_loss = []
 
-for epoch in range(epochs):
-    model.train()
-    
-    for step, (anchor_point, pos_point, neg_point, anchor_label) in enumerate(train_loader):
-        anchor_point = anchor_point.to(device, dtype=torch.float)
-        pos_point = pos_point.to(device, dtype=torch.float)
-        neg_point = neg_point.to(device, dtype=torch.float)
+def train_model():
+    # Training loop
+    for epoch in range(epochs):
+        running_loss = []
+        model.train()
         
-        optimizer.zero_grad()
+        for step, (anchor_point, pos_point, neg_point, anchor_label) in enumerate(train_loader):
+            anchor_point = anchor_point.to(device, dtype=torch.float)
+            pos_point = pos_point.to(device, dtype=torch.float)
+            neg_point = neg_point.to(device, dtype=torch.float)
+            
+            optimizer.zero_grad()
 
-        anchor_out = model(anchor_point)
-        positive_out = model(pos_point)
-        negative_out = model(neg_point)
+            anchor_out = model(anchor_point)
+            positive_out = model(pos_point)
+            negative_out = model(neg_point)
+            
+            loss = criterion(anchor_out, positive_out, negative_out)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss.append(loss.cpu().detach().numpy())
+
+        # train_acc = 100*evaluate_accuracy(train_loader, model.to(device))
+        # train_accs.append(train_acc)
+        print("Epoch: {}/{} - Loss: {:.4f}".format(epoch+1, epochs, np.mean(running_loss)))
+        # print('Accuracy of train set: {:.00f}%'.format(train_acc))
+        print('')
+
+
+
+# train_model()
+
+# Save model
+torch.save({"model_state_dict": model.state_dict(),
+            "optimzier_state_dict": optimizer.state_dict()
+           }, "trained_model.pth")
+
+
+
+# # Evaluate model
+# train_results = []
+# labels = []
+
+# model.eval()
+# with torch.no_grad():
+#     for anchor, _, _, label in train_loader:
+#         train_results.append(model(anchor.to(device, dtype=torch.float)).cpu().numpy())
+#         labels.append(label)
         
-        loss = criterion(anchor_out, positive_out, negative_out)
-        loss.backward()
-        optimizer.step()
-        
-        running_loss.append(loss.cpu().detach().numpy())
+# train_results = np.concatenate(train_results)
+# labels = np.concatenate(labels)
+# train_results.shape
 
-    train_acc = 100*evaluate_accuracy(train_loader, model.to(device))
-    train_accs.append(train_acc)
-    print("Epoch: {}/{} - Loss: {:.4f}".format(epoch+1, epochs, np.mean(running_loss)))
-    print('Accuracy of train set: {:.00f}%'.format(train_acc))
-    print('')
+# print(labels)
 
+# plt.figure(figsize=(15, 10), facecolor="azure")
+# for label in np.unique(labels):
+#     tmp = train_results[labels==label]
+#     plt.scatter(tmp[:, 0], tmp[:, 1], label=label)
 
-
-
-
-
-
-
-# # Define list to store loss of each iteration
-# train_losses = []
-# train_accs = []
-
-
-
-# for i, (data, labels) in enumerate(trainloader):
-#   print(data.shape, labels.shape)
-#   print(data)
-#   if i == 3:
-#       break
-
-
-
-# # Training loop
-# model.train()
-# for epoch in tqdm(range(epochs), desc="Epochs"):
-#   running_loss = []
-#   for step, (anchor_point, pos_point, neg_point, anchor_label) in enumerate(tqdm(train_loader, desc="Trainig", leave=False))
-
-# # Training loop
-# for epoch in range(epochs):
-#     # Training loop
-#     for i, (x_batch, y_batch) in enumerate(trainloader):
-#         # Convert labels to one-hot encoding
-#         y_batch = nn.functional.one_hot(y_batch, num_classes=10)
-
-#         # Perform forward pass
-#         y_pred = model.forward(x_batch)
-
-#         loss, grad = tcn.CrossEntropyLoss(y_batch, y_pred)
-#         train_losses.append(loss)
-        
-#         model.backward(grad)
-#         model.optimizer_step(learning_rate)
-
-#         # Calculate accuracy of prediction
-#         correct = torch.argmax(y_pred, axis=1) == torch.argmax(y_batch, axis=1)
-#         train_accs.append(torch.sum(correct)/len(y_pred))
-
-#         # Print progress
-#         if i % 200 == 0:
-#             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-#                 epoch + 1, i * len(x_batch), len(trainloader.dataset),
-#                 100. * i / len(trainloader), loss))
+# plt.legend()
+# plt.show()
