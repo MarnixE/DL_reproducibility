572b35af78ec572ef432e735fc878d23178e651c
Marcus_Malak <m.m.h.g.malak@student.tudelft.nl>
2022-04-14 15:26:34 +0200

    still fixing haars

--- drive2vec/model/tcn.py
+++ drive2vec/model/tcn.py
@@ -93,31 +93,34 @@ class TCN(nn.Module):
         self.wv_linear = nn.Linear(n_channels[-1]*2, n_outputs)
 
     def haar_wavelet(data):
-        col = data.shape[1]
+        col = data.shape[2]
         # print("col", col)
-        row = data.shape[0]
+        row = data.shape[1]
         # print("col", row)
-        # d3= data.shape[0]
+        d3= data.shape[0]
         # print("col", d3)
-        # finaldata= np.empty((d3,row*2,col))
+        finaldata= np.empty((d3,row*2,col))
         print(data.shape)
 
+        for j in range(d3):
+            new_data=data[j,:,:].copy()
+            for i in range(row):
+                new_row = data[i,:].copy()
+                print(new_row.shape)
+                (cA, cD) = pywt.dwt(new_row, 'haar')
+                # print(new_col.shape)
+                print(cA.shape)
+                print(cD.shape)
+                new_data1 = np.concatenate((cA,cD),0)
+                print(new_data1.shape)
+                new_data2 = np.reshape(new_data1,(-1,col))
+                print(new_data2.shape)
+                new_data = np.vstack((data,new_data2))
+                print(new_data.shape)
+            finaldata[j,:,:]=new_data
+        print(finaldata.shape)
 
-        for i in range(row):
-            new_row = data[i,:].copy()
-            # print(new_row.shape)
-            (cA, cD) = pywt.dwt(new_row, 'haar')
-            # print(new_col.shape)
-            # print(cA.shape)
-            # print(cD.shape)
-            new_data1 = np.concatenate((cA,cD),0)
-            # print(new_data1.shape)
-            new_data2 = np.reshape(new_data1,(-1,col))
-            print(new_data2.shape)
-            new_data = np.vstack((data,new_data2))
-            print(new_data.shape)
-
-        return new_data
+        return finaldata
 
 
     def forward(self, x):
